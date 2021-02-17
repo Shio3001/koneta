f1a3e6f28138f1f5f8c56372742d96a4d1a08e64
Shio3001 <taku2000maru@icloud.com>
2021-02-17 13:10:21 +0900

    クレペリン？？？

--- 99.py
+++ 99.py
@@ -5,13 +5,30 @@ print("九九計算して")
 def loop():
     i1 = random.randint(0, 9)
     i2 = random.randint(0, 9)
-    correct = i1 * i2
 
-    print("{0} x {1}".format(i1, i2))
+    m = random.randint(0, 3)
 
-    ans = int(input().rstrip())
+    correct = 0
 
-    if correct != ans:
+    if m == 0:
+        correct = i1 * i2
+        print("{0} x {1}".format(i1, i2))
+
+    if m == 1:
+        correct = i1 + i2
+        print("{0} + {1}".format(i1, i2))
+
+    if m == 2:
+        correct = i1 - i2
+        print("{0} - {1}".format(i1, i2))
+
+    correct_str = str(correct)
+    ans_str = input().rstrip()
+
+    correct_f = correct_str[-1]
+    ans_f = ans_str[-1]
+
+    if correct_f != ans_f:
         return
 
     loop()
